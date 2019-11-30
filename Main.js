40b331c80795d5570380130f6f361ed36ad86718
ErvinPaulES <ErvinPaulES@github.com>
2019-11-29 23:40:49 -0600

    Terminado

---
+++ Main.js
@@ -0,0 +1,27 @@
+import Componentes from './Componentes.js';
+import View from './View.js';
+
+class Main{
+    constructor(){
+        let view = new View(document.querySelector('#preorder'), document.querySelector('#posorder'));
+        document.querySelector('#agregar').addEventListener('click', () => {
+                let valores = new Array();
+                let operacion = document.querySelector('#operacion').value;
+                for(i=0;i<operacion.length;i++){
+                    if(operacion[i]!='+' && operacion[i]!='-'&& operacion[i]!='/'&& operacion[i]!='*'){
+                        valores[i] = parseFloat(operacion[i])
+                    }else{
+                        valores[i] = operacion[i];
+                    }
+                    
+                }
+                for(i=0;i<valores.length;i++){
+                    let componente = new Componente(valores[i]);
+                    view.agregarComponente(componente);
+                }
+                
+            
+        })
+    }
+}
+let main = new Main();
---
+++ View.js
@@ -0,0 +1,11 @@
+export default class View{
+    constructor(preorder, posorder){
+        this._preorder = preorder;
+        this._posorder = posorder;
+        this._Componentes = new Array();
+
+    }
+    agregarComponente(componente){
+        this._Componentes.push(componente);
+    }
+}
